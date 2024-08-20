ba5dc89142402be6c04d72c34a469fc27ff0207b
dstrzykalski <dstrzykalski@yahoo.com>
2024-08-20 18:13:05 -0400

    Server and Routes Commit

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,20 +20,58 @@ router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then(data => {
+    res.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
     
   
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then(data => {
+    res.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    res.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    res.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
