00e0aff57fe5d272e3c1c2c6f272f9363895e2ef
dstrzykalski <dstrzykalski@yahoo.com>
2024-08-30 16:00:28 -0400

    Fixed With Help From Tutor

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,7 +27,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  product.findOne({
+  Product.findOne({
     where: {
       //id is the assigned value of the id parameter
       id: req.params.id
